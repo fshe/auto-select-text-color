34c35bd76ac4973b937159782fe0bac310be27b2
Frank She <33668602+fshe@users.noreply.github.com>
2020-01-28 13:56:22 -0800

    Updates

---
+++ precache-manifest.0afac8fb0e7badbf38679bfea8fc659a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06974e1aaec381f73a5953c6d41e359e",
+    "url": "/auto-select-text-color/index.html"
+  },
+  {
+    "revision": "45c2165aeea0367aed91",
+    "url": "/auto-select-text-color/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bcad7e8a8905c395af0d",
+    "url": "/auto-select-text-color/static/js/2.06a0370a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/auto-select-text-color/static/js/2.06a0370a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45c2165aeea0367aed91",
+    "url": "/auto-select-text-color/static/js/main.31f01cf5.chunk.js"
+  },
+  {
+    "revision": "b5c8724ceae47500159e",
+    "url": "/auto-select-text-color/static/js/runtime-main.e0c0f2e1.js"
+  }
+]);
